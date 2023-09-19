eaf545eab579306e0c6b08ca8f941c71d76f2318
Elootam <toma.lion@hotmail.fr>
2023-09-19 14:30:21 +0200

    bomb commit

---
+++ Intermediate/Build/Win64/x64/LD_IMPLEMENTATION/Development/Core/SharedPCH.Core.ShadowErrors.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.36.32537
+#include "C:/Users/cleme/Documents/Pro/ENJMIN/JMIN-M2-LD-IMPLEMENTATION/LD_IMPLEMENTATION/Intermediate/Build/Win64/x64/LD_IMPLEMENTATION/Development/Core/SharedPCH.Core.ShadowErrors.InclOrderUnreal5_0.h"
---
+++ Intermediate/Source/LD_IMPLEMENTATION.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class LD_IMPLEMENTATION : ModuleRules
+{
+	public LD_IMPLEMENTATION(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
